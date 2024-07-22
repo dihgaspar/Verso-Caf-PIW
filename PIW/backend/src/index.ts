d7ca2cd20f4b7d3bf4ecb1511ecf3bbc1b305731
Diogo Gaspar <159595759+dihgaspar@users.noreply.github.com>
2024-07-22 09:45:33 -0300

    Atualização do Projeto - Entrega 01
    Adição da pasta backend e realização das configurações solicitadas.

---
+++ PIW/backend/src/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('*', (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <title>Projeto Final</title>
+      </head>
+      <body>
+        <h1>Verso & Café é um cafeteria, que oferece aos seus clientes excelentes opções de bebidas, lanches e livros!</p>
+        <h2>Autores</h2>
+        <ul>
+          <li>Diogo Gaspar</li>
+          <li>José Aurivanio</li>
+        </ul>
+      </body>
+    </html>
+  `);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
